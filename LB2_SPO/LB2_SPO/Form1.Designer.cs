f25bd059c7defa995cbca884401b6bc3e68071dd
alexit777 <90395600+alexit777@users.noreply.github.com>
2021-09-17 16:09:09 +0300

    1

---
+++ LB2_SPO/LB2_SPO/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace LB2_SPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(569, 224);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Название";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Показания";
+            this.Column2.Name = "Column2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 352);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 385);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 277);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Информация о памяти";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LB2_SPO/LB2_SPO/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+using System.Linq.Expressions;
+using System.Management;
+
+namespace LB2_SPO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            load();
+            //GetSystemInfo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        [DllImport("kernel32.dll")]
+        
+        public static extern IntPtr GlobalAlloc(int con, int size);
+        [DllImport("kernel32.dll")]
+        
+        public static extern int GlobalFree(IntPtr start);
+        [DllImport("kernel32.dll")]
+        public static extern void GlobalMemoryStatus(ref MEMORYSTATUS lpBuffer);
+        //
+        public struct MEMORYSTATUS
+        {
+            public UInt32 dwLength;               
+            public UInt32 dwMemoryLoad;           
+            public UInt32 dwTotalPhys;            
+            public UInt32 dwAvailPhys;            
+            public UInt32 dwTotalPageFile;        
+            public UInt32 dwAvailPageFile;        
+            public UInt32 dwTotalVirtual;         
+            public UInt32 dwAvailVirtual;         
+            public UInt32 dwAvailExtendedVirtual;
+        }
+        public void load()
+        {
+            
+            MEMORYSTATUS memStatus = new MEMORYSTATUS();
+         
+            GlobalMemoryStatus(ref memStatus);
+
+
+            dataGridView1.Rows.Add("Размер структуры", memStatus.dwLength.ToString());
+            dataGridView1.Rows.Add("Процент занятой памяти", memStatus.dwMemoryLoad.ToString() + " %");
+            dataGridView1.Rows.Add("Общее кол-во физической памяти", memStatus.dwTotalPhys.ToString() + " byte");
+            dataGridView1.Rows.Add("Свободное кол-во физической памяти", memStatus.dwAvailPhys.ToString() + " byte");
+            dataGridView1.Rows.Add("Предел памяти для системи", memStatus.dwTotalPageFile.ToString() + " byte");
+            dataGridView1.Rows.Add("Максимальный объем памяти", memStatus.dwAvailPageFile.ToString() + " byte");
+            dataGridView1.Rows.Add("Общее количество виртуальной памяти", memStatus.dwTotalVirtual.ToString() + " byte");
+            dataGridView1.Rows.Add("Свободное количество виртуальной памяти", memStatus.dwAvailVirtual.ToString() + " byte");
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            load();
+        }
+
+        void GetSystemInfo()
+        {
+            ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_LogicalMemoryConfiguration");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
+            foreach (ManagementObject item in searcher.Get())
+            {
+                label2.Text = item["TotalPhysicalMemory"].ToString();
+                label4.Text = item["TotalVirtualMemory"].ToString();
+                label6.Text = item["TotalPageFileSpace"].ToString();
+                label8.Text = item["AvailableVirtualMemory"].ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
